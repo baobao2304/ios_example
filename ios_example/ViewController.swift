278128ccd62e87147e5e7301af4aec09eded9751
Bao Pham <bao.pvl@mmh.co.jp>
2024-11-10 17:56:30 +0700

    feat: build the first screen start ship

--- ios_example/ViewController.swift
+++ ios_example/ViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
     var isYellow: Bool = false
+    var player: AVAudioPlayer!
+    @IBOutlet weak var ship: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = Bundle.main.path(forResource: "spaceshipfly", ofType: "wav")
+        let url = URL(fileURLWithPath: path!)
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        }catch let error as NSError{
+            print(error.description)
+        }
     }
 
     @IBAction func onTapButtonEx1(_ sender: Any) {
@@ -25,5 +35,26 @@ class ViewController: UIViewController {
            }
     }
     
+
+    
+    @IBAction func onButtonPressed(_ sender: Any) {
+    
+        if isYellow{
+            self.ship.isHidden = true
+            UIView.animate(withDuration: 1, animations: {
+                self.ship.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+            })
+               isYellow = false
+           }else{
+               self.player.play()
+               self.ship.isHidden = false
+               UIView.animate(withDuration: 3, animations: {
+                   self.ship.frame = CGRect(x: 50, y: 300, width: 300, height: 200)
+               })
+               isYellow = true
+           }
+     
+        
+    }
 }
 
